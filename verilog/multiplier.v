1c92224eacaf9dca941a5807e6f46c3cb3a4aebf
QingmuDeng <qingmu.deng@students.olin.edu>
2018-11-12 19:22:12 -0500

    multiplier verilog file

---
+++ verilog/multiplier.v
@@ -0,0 +1,15 @@
+module multiply (
+  input enable,
+  input [31:0] A,
+  input [31:0] B,
+  output reg [31:0] Hi,
+  output reg [31:0] Lo
+  );
+
+  always @ ( * ) begin
+    if(enable) begin
+      {Hi, Lo} = A*B;
+    end
+  end
+
+endmodule // multiply
